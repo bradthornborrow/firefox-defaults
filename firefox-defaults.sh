b3964f1471a7c6eadd01b86933b0e53d9a0b3bb2
Brad Thornborrow <brad@thedeskofbrad.ca>
2023-11-27 13:05:21 -0500

    Initial commit

---
+++ firefox-defaults.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Set source path to current path 
+SOURCE_PATH=$(pwd)
+
+# Change to default Firefox config directory
+if [ -d "$HOME/Library/Application Support/Firefox" ]; then
+	cd ~/Library/Application\ Support/Firefox
+else
+	cd -- "$(find $HOME -ipath "*/.mozilla/firefox" -type d -printf '%h' -quit)"/firefox
+fi
+
+# Locate default Firefox profile directory
+if [[ $(grep '\[Profile[^0]\]' profiles.ini) ]]
+	then PROFPATH=$(grep -E '^\[Profile|^Path|^Locked' profiles.ini | grep -1 '^Locked=1' | grep '^Path' | cut -c6-)
+	else PROFPATH=$(grep 'Path=' profiles.ini | sed 's/^Path=//')
+fi
+
+# Copy preferences to default Firefox profile folder
+cp -R $SOURCE_PATH/firefox-defaults/* $PROFPATH/
