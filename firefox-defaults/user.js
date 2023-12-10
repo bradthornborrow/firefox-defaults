01bafc092cfd5cec2ad6c65d4bbc820f199ab269
Brad Thornborrow <brad@thedeskofbrad.ca>
2023-12-10 17:32:12 -0500

    Adjust touchpad sensitivity

--- firefox-defaults/user.js
+++ firefox-defaults/user.js
@@ -6,12 +6,12 @@ user_pref("apz.gtk.kinetic_scroll.enabled", true);
 user_pref("apz.overscroll.enabled", true);
 user_pref("general.autoScroll", true);
 user_pref("general.smoothScroll", true);
-user_pref("general.smoothScroll.mouseWheel.durationMaxMS", 500);
-user_pref("general.smoothScroll.mouseWheel.durationMinMS", 250);
+user_pref("general.smoothScroll.mouseWheel.durationMaxMS", 200);
+user_pref("general.smoothScroll.mouseWheel.durationMinMS", 200);
 user_pref("mousewheel.acceleration.start", 1);
-user_pref("mousewheel.default.delta_multiplier_x", 15);
-user_pref("mousewheel.default.delta_multiplier_y", 15);
-user_pref("mousewheel.default.delta_multiplier_z", 15);
+user_pref("mousewheel.default.delta_multiplier_x", 10);
+user_pref("mousewheel.default.delta_multiplier_y", 10);
+user_pref("mousewheel.default.delta_multiplier_z", 10);
 //
 // Enable User Chrome customizations
 //
