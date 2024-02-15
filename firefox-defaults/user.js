cd47db4006154094951da79746eb8d47b32fc0eb
Brad Thornborrow <brad@thedeskofbrad.ca>
2024-02-15 18:49:54 -0500

    Update user.js

--- firefox-defaults/user.js
+++ firefox-defaults/user.js
@@ -2,6 +2,8 @@
 //
 // Enable Mac-like smooth scrolling
 //
+user_pref("apz.overscroll.enabled", true);
+user_pref("general.smoothScroll.mouseWheel.durationMaxMS", 500);
 user_pref("general.smoothScroll.mouseWheel.durationMinMS", 200);
 user_pref("mousewheel.acceleration.start", 1);
 user_pref("mousewheel.default.delta_multiplier_x", 50);
