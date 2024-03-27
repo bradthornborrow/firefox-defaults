7861334557aa3a4ce793615657f1a8da8febb91b
Brad Thornborrow <brad@thedeskofbrad.ca>
2024-03-26 21:14:14 -0400

    Update config for Windows

--- firefox-defaults.ps1
+++ firefox-defaults.ps1
@@ -1,6 +1,6 @@
 Get-ChildItem -Directory -Path "$Env:USERPROFILE\AppData\Roaming\Mozilla\Firefox\Profiles" -Filter '*.default-release' | % {
    # copy contents to *.default-release folder
    if($_.Name -match "\.default-release$"){
-      Copy-Item -Path .\Firefox-Defaults\* -Destination $_.FullName -recurse
+      Copy-Item -Path .\Firefox-Defaults\* -Destination $_.FullName -recurse -force
   }
 }
