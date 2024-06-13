dbdb79596cd8c1db56acd9824b7fbcd3a11a4f76
Brad Thornborrow <brad@thedeskofbrad.ca>
2024-06-12 22:01:37 -0400

    Update user.js

--- firefox-defaults/user.js
+++ firefox-defaults/user.js
@@ -2,15 +2,9 @@
 //
 // Enable Mac-like smooth scrolling
 //
-user_pref("apz.overscroll.enabled", true);
-user_pref("general.autoScroll", true);
-user_pref("general.smoothScroll.mouseWheel.durationMaxMS", 500);
-user_pref("general.smoothScroll.mouseWheel.durationMinMS", 250);
-user_pref("mousewheel.acceleration.start", 1);
-// user_pref("mousewheel.acceleration.factor", 5);
-// user_pref("mousewheel.default.delta_multiplier_x", 50);
-// user_pref("mousewheel.default.delta_multiplier_y", 50);
-// user_pref("mousewheel.default.delta_multiplier_z", 50);
+user_pref("mousewheel.default.delta_multiplier_x", 90);
+user_pref("mousewheel.default.delta_multiplier_y", 90);
+user_pref("mousewheel.default.delta_multiplier_z", 90);
 //
 // Enable User Chrome customizations
 //
