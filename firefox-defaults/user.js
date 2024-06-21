0730dd7fba23c88fb83d07da3b67e8c02d08d35d
Brad Thornborrow <brad@thedeskofbrad.ca>
2024-06-21 12:11:41 -0400

    Update user.js for Ubuntu Budgie

--- firefox-defaults/user.js
+++ firefox-defaults/user.js
@@ -2,9 +2,9 @@
 //
 // Enable Mac-like smooth scrolling
 //
-user_pref("mousewheel.default.delta_multiplier_x", 90);
-user_pref("mousewheel.default.delta_multiplier_y", 90);
-user_pref("mousewheel.default.delta_multiplier_z", 90);
+user_pref("mousewheel.default.delta_multiplier_x", 75);
+user_pref("mousewheel.default.delta_multiplier_y", 75);
+user_pref("mousewheel.default.delta_multiplier_z", 75);
 //
 // Enable User Chrome customizations
 //
