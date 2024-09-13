a2a32114850a0d679a5410d6ebd8b08ff586ae26
Brad Thornborrow <brad@thedeskofbrad.ca>
2024-09-12 21:31:53 -0400

    Update user.js

--- firefox-defaults/user.js
+++ firefox-defaults/user.js
@@ -1,5 +1,15 @@
 // Mozilla User Preferences
 //
+// Mac-like touchpad scrolling for Linux
+// 
+// general.smoothScroll.mouseWheel.durationMaxMS=500
+// general.smoothScroll.mouseWheel.durationMinMS=250
+// mousewheel.acceleration.start=1
+// mousewheel.default.delta_multiplier_x=10
+// mousewheel.default.delta_multiplier_y=10
+// mousewheel.default.delta_multiplier_z=10
+//
+//
 // Enable User Chrome customizations
 //
 user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
