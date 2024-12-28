79d6913d9762e7d5958ba5f6b07c2b9ec9633c68
Brad Thornborrow <brad@thedeskofbrad.ca>
2024-12-27 22:54:55 -0500

    Update user.js

--- firefox-defaults/user.js
+++ firefox-defaults/user.js
@@ -5,11 +5,14 @@
 // user_pref("general.smoothScroll.mouseWheel.durationMaxMS", 500);
 // user_pref("general.smoothScroll.mouseWheel.durationMinMS", 250);
 // user_pref("mousewheel.acceleration.start", 1);
-// user_pref("mousewheel.default.delta_multiplier_x", 25);
-// user_pref("mousewheel.default.delta_multiplier_y", 25);
-// user_pref("mousewheel.default.delta_multiplier_z", 25);
+user_pref("mousewheel.default.delta_multiplier_x", 50);
+user_pref("mousewheel.default.delta_multiplier_y", 50);
+user_pref("mousewheel.default.delta_multiplier_z", 50);
 //
 // Enable User Chrome customizations
 //
 user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
+//
+// Enable custom Search engines
+user_pref("browser.urlbar.update2.engineAliasRefresh", true);
 //
